7a1e25de5e95e93c6287d610e9d71e77359f31d0
chazzlee <chazz@dev.com>
2022-12-18 16:51:14 -0500

    add cors for development

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+const cors = require("cors");
 
+const { isProduction } = require("./config/keys");
 const usersRouter = require("./routes/api/users");
 const tweetsRouter = require("./routes/api/tweets");
 
@@ -11,6 +13,9 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+if (!isProduction) {
+  app.use(cors());
+}
 
 app.use("/api/users", usersRouter);
 app.use("/api/tweets", tweetsRouter);
