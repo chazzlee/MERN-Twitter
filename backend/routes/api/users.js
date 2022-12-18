6c58e4186c76ea9c73e1e24aad0f4e9eb70a35fc
chazzlee <chazz@dev.com>
2022-12-18 17:21:02 -0500

    create user registration endpoint

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -1,9 +1,51 @@
 const express = require("express");
+const bcrypt = require("bcryptjs");
+const mongoose = require("mongoose");
+const User = mongoose.model("User");
+
 const router = express.Router();
 
-/* GET users listing. */
+/* GET /api/users */
 router.get("/", (req, res, next) => {
   res.json({ message: "GET /users" });
+});
+
+/* POST /api/users/register */
+router.post("/register", async (req, res, next) => {
+  const { email, username, password } = req.body;
+  const user = await User.findOne({
+    $or: [{ email }, { username }],
+  });
+
+  if (user) {
+    const error = new Error("Validation Error");
+    error.statusCode = 400;
+    const errors = {};
+    if (user.email === email) {
+      errors.email = "A user has already registered with this email";
+    }
+    if (user.username === username) {
+      errors.username = "A user has already registered with this username";
+    }
+    error.errors = errors;
+    return next(error);
+  }
+
+  const newUser = new User({ username, email });
+
+  bcrypt.genSalt(10, (err, salt) => {
+    if (err) throw err;
+    bcrypt.hash(password, salt, async (err, hashedPassword) => {
+      if (err) throw err;
+      try {
+        newUser.hashedPassword = hashedPassword;
+        const user = await newUser.save();
+        return res.json({ user });
+      } catch (err) {
+        next(err);
+      }
+    });
+  });
 });
 
 module.exports = router;
