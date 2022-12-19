b6c165d5d75a68da12132869d51554b5013ff7d7
chazzlee <chazz@dev.com>
2022-12-18 20:20:36 -0500

    finish frontend

---
+++ frontend/src/components/Tweets/TweetBox.jsx
@@ -0,0 +1,9 @@
+export default function TweetBox({ text, username }) {
+  return (
+    <div className="tweet">
+      <h3>
+        {username ? `${username}:` : ""} {text}{" "}
+      </h3>
+    </div>
+  );
+}
